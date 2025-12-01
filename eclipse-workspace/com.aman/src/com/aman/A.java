4faa9e4e8e73711a6a57fcbc24591c25f34a0965
aman <aman21012002@gmail.com>
2025-12-01 17:32:02 +0530

    submit project

---
+++ eclipse-workspace/com.aman/src/com/aman/A.java
@@ -0,0 +1,19 @@
+
+package com.aman;
+
+class A{
+	public static void main(String[] args) {
+		
+		int arr[] = {10,11,12,14,30,40};
+		
+		int sum=0;
+		for(int i=0; i<arr.length;  i++ ) {
+			 sum=sum+arr[i];
+				System.out.println(sum);
+			}
+		}
+	}
+
+		
+ 
+
---
+++ eclipse-workspace/com.aman/src/com/aman/Demo.java
@@ -0,0 +1,17 @@
+
+package com.aman;
+
+class Demo{
+	public static void main(String[] args) {
+		
+		int arr[] = {10,11,12,14,30,40};
+		for(int i=0; i<arr.i++ ) {
+			if(maks[i]<35) {
+				System.out.println(arr[]
+			}
+		}
+	}
+}
+		
+ 
+
