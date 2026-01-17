8e03a50647c2684d80a968e9a11d6c0ebcea8f62
aman <aman21012002@gmail.com>
2026-01-17 14:01:55 +0530

    "Pattern"

---
+++ eclipse-workspace/Java_A4/src/pattern/F.java
@@ -0,0 +1,25 @@
+package pattern;
+
+public class F {
+
+	  public static void main(String[] args) {
+
+		  
+
+		        int n = 11;
+		        
+
+		        for (int i = 0; i <= n; i++) {
+		            for (int j = 0; j <= n; j++) {
+
+		                if(j==0 || i==0 || i==(n-1)/2 ) {
+		                    System.out.print("*");
+		                } else {
+		                    System.out.print(" ");
+		                }
+		            }
+		            System.out.println();
+		        }
+		    }
+
+}
